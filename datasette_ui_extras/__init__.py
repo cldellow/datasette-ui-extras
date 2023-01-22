3cfeb84387d6b716a2c592438e63f4c59510a6bf
Colin Dellow <cldellow@gmail.com>
2023-01-22 17:23:18 -0500

    rename app.css
    Rough idea: can we isolate each UI thing to its own CSS/JS file?
    
    This is handy for development, and might make it possible for a user
    to selectively enable/disable treatments.
    
    We'd eventually want to concatenate (and ideally minify) them all into
    a single file to minimize impact of making many requests, but we
    can ignore that until its in the 10-20 range.

--- datasette_ui_extras/__init__.py
+++ datasette_ui_extras/__init__.py
@@ -6,11 +6,11 @@ from datasette import hookimpl
 @hookimpl
 def extra_css_urls(datasette):
     return [
-        datasette.urls.static_plugins("datasette-ui-extras", "app.css")
+        datasette.urls.static_plugins("datasette-ui-extras", "facet-treatment.css")
     ]
 
 @hookimpl
 def extra_js_urls(datasette):
     return [
-        datasette.urls.static_plugins("datasette-ui-extras", "fixed-table-headers.js")
+        datasette.urls.static_plugins("datasette-ui-extras", "sticky-table-headers.js")
     ]
