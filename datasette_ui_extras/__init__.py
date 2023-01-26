b3ee4c6b1f20698b5e735cdb926fe5541bee11e0
Colin Dellow <cldellow@gmail.com>
2023-01-25 23:50:22 -0500

    ensure sticky thead has border

--- datasette_ui_extras/__init__.py
+++ datasette_ui_extras/__init__.py
@@ -1,12 +1,10 @@
 from datasette import hookimpl
 
-from datasette import hookimpl
-
-
 @hookimpl
 def extra_css_urls(datasette):
     return [
-        datasette.urls.static_plugins("datasette-ui-extras", "facet-treatment.css")
+        datasette.urls.static_plugins("datasette-ui-extras", "facet-treatment.css"),
+        datasette.urls.static_plugins("datasette-ui-extras", "sticky-table-headers.css"),
     ]
 
 @hookimpl
