188012f9a6c054e7d6dea245a2427b7ae9c10fbe
Colin Dellow <cldellow@gmail.com>
2023-02-05 18:09:50 -0500

    serve concatenated css/js, fix #51

--- datasette_ui_extras/__init__.py
+++ datasette_ui_extras/__init__.py
@@ -1,4 +1,6 @@
 import json
+import os
+import hashlib
 import datasette
 import asyncio
 import markupsafe
@@ -12,38 +14,70 @@ from .utils import row_edit_params
 
 PLUGIN = 'datasette-ui-extras'
 
+css_files = [
+    "app.css",
+    "hide-export.css",
+    "hide-table-definition.css",
+    "sticky-table-headers.css",
+    "lazy-facets.css",
+    "hide-filters.css",
+    "layout-row-page.css",
+    "compact-cogs.css",
+    "mobile-column-menu.css",
+    "edit-row.css",
+]
+
+js_files = [
+    'hide-filters.js',
+    "sticky-table-headers.js",
+    "focus-search-box.js",
+    'lazy-facets.js',
+    'layout-row-page.js',
+    "mobile-column-menu.js",
+    "edit-row.js",
+]
+
+def fingerprint(files, ext):
+    def concatenate():
+        rv = []
+        for fname in files:
+            rv.append('/* {} */'.format(fname))
+            fpath = os.path.abspath(os.path.join(__file__, '..', 'static', fname))
+
+            f = open(fpath, 'r')
+            contents = f.read()
+            f.close()
+            rv.append(contents)
+
+        return '\n\n'.join(rv)
+
+    hashcode = hashlib.sha256(concatenate().encode('utf-8')).hexdigest()[0:8]
+
+    # TODO: how can we distinguish prod vs dev so we can serve a fingerprinted,
+    #       long-lived cached file in prod, but have live reload in dev?
+    #path = '/-/{}/{}.{}'.format(PLUGIN, hashcode, ext)
+    path = '/-/{}/{}.{}'.format(PLUGIN, ext, ext)
+
+    return path, concatenate
+
+css_path, css_contents = fingerprint(css_files, 'css')
+js_path, js_contents = fingerprint(js_files, 'js')
+
 # Not fully fleshed out: datasette-ui-extras consists of a bunch of "extras".
 # Each extra has one or more of: custom CSS, custom JS, custom Python.
 #
 # Try to develop them as though they're standalone things, so they can be
 # easily turned on/off, or upstreamed into Datasette.
-#
-# TODO: serve a minified/concatenated CSS/JS file
 @datasette.hookimpl
 def extra_css_urls(datasette):
     return [
-        datasette.urls.static_plugins(PLUGIN, "app.css"),
-        datasette.urls.static_plugins(PLUGIN, "hide-export.css"),
-        datasette.urls.static_plugins(PLUGIN, "hide-table-definition.css"),
-        datasette.urls.static_plugins(PLUGIN, "sticky-table-headers.css"),
-        datasette.urls.static_plugins(PLUGIN, "lazy-facets.css"),
-        datasette.urls.static_plugins(PLUGIN, "hide-filters.css"),
-        datasette.urls.static_plugins(PLUGIN, "layout-row-page.css"),
-        datasette.urls.static_plugins(PLUGIN, "compact-cogs.css"),
-        datasette.urls.static_plugins(PLUGIN, "mobile-column-menu.css"),
-        datasette.urls.static_plugins(PLUGIN, "edit-row.css"),
+        css_path
     ]
 
 @datasette.hookimpl(tryfirst=True)
 def extra_js_urls(datasette):
     return [
-        datasette.urls.static_plugins(PLUGIN, 'hide-filters.js'),
-        datasette.urls.static_plugins(PLUGIN, "sticky-table-headers.js"),
-        datasette.urls.static_plugins(PLUGIN, "focus-search-box.js"),
-        datasette.urls.static_plugins(PLUGIN, 'lazy-facets.js'),
-        datasette.urls.static_plugins(PLUGIN, 'layout-row-page.js'),
-        datasette.urls.static_plugins(PLUGIN, "mobile-column-menu.js"),
-        datasette.urls.static_plugins(PLUGIN, "edit-row.js"),
+        js_path
     ]
 
 @datasette.hookimpl
@@ -119,3 +153,22 @@ def sniff_actor_from_request(datasette, request):
 
     # all corresponding hookimpls are invoked here
     outcome = yield
+
+@datasette.hookimpl
+def register_routes():
+    return [
+        (
+            css_path,
+            lambda: datasette.Response(
+                css_contents(),
+                content_type="text/css; charset=utf-8"
+            )
+        ),
+        (
+            js_path,
+            lambda: datasette.Response(
+                js_contents(),
+                content_type="text/javascript; charset=utf-8"
+            )
+        ),
+    ]
