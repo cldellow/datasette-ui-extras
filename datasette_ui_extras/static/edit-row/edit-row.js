106937f3b8fd4e2a93f0746ff8dc32bc1a173a46
Colin Dellow <cldellow@gmail.com>
2023-02-14 22:27:11 -0500

    can delete rows, fixes #73

--- datasette_ui_extras/static/edit-row/edit-row.js
+++ datasette_ui_extras/static/edit-row/edit-row.js
@@ -90,33 +90,100 @@
 // Add an "Edit row" button on the regular row page
 (function() {
   function initialize() {
-    // Only run on the row page, not the edit-row page
-    if (!document.body.classList.contains('row') || document.body.classList.contains('edit-row'))
+    // Only run on the row page and edit-row pages
+    if (!document.body.classList.contains('row'))
       return;
 
-    // TODO: check if user has update-row permission; maybe we embed
-    //       some _dux_permissions constants via a extra_body_script?
-    if (!__dux_permissions['update-row'])
-      return;
+    let needsEdit = false;
+    let needsDelete =false;
+
+    if (!document.body.classList.contains('edit-row') && __dux_permissions['update-row'])
+      needsEdit = true;
+
+    if (__dux_permissions['delete-row'])
+      needsDelete = true;
 
     const h1 = document.querySelector('h1');
 
     if (!h1)
       return;
 
-    const form = document.createElement('form');
-    const button = document.createElement('input');
-    button.type = 'submit';
-    button.value = 'Edit data';
-    form.appendChild(button);
-    h1.after(form);
+    if (needsDelete) {
+      const form = document.createElement('form');
+      form.classList.add('btn-row-actions');
+      const button = document.createElement('input');
+      button.classList.add('btn-delete');
+      button.type = 'submit';
+      button.value = 'Delete row';
+      form.appendChild(button);
+      h1.after(form);
 
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const url = new URL(window.location.href);
-      url.searchParams.append('_dux_edit', '1');
-      window.location = url.toString();
-    });
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const really = confirm("Deleting is permanent. Really delete?");
+
+        if (!really)
+          return;
+
+        const redirectAfterEditEndpoint = new URL(window.location.href);
+        redirectAfterEditEndpoint.pathname += '/-/dux-redirect-after-edit';
+        redirectAfterEditEndpoint.searchParams.set('action', 'delete-row');
+
+        const deleteEndpoint = new URL(window.location.href);
+        deleteEndpoint.pathname += '/-/delete';
+
+        try {
+          {
+            const response = await fetch(deleteEndpoint.toString(), {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                })
+            });
+            const data = await response.json();
+            if (!data.ok) {
+              alert(`Error: ${data.errors.join(', ')}`);
+              return;
+            }
+          }
+
+          {
+            const redirectResponse = await fetch(redirectAfterEditEndpoint.toString(), {
+                method: 'GET',
+            });
+            const data = await redirectResponse.json();
+            window.location.href = data.url;
+          }
+
+        } catch(e) {
+          console.error(e);
+          alert(`An error occurred while deleting the row: ${JSON.stringify(e)}`);
+        }
+
+      });
+    }
+
+    if (needsEdit) {
+      const form = document.createElement('form');
+      form.classList.add('btn-row-actions');
+      const button = document.createElement('input');
+      button.type = 'submit';
+      button.value = 'Edit row';
+      form.appendChild(button);
+      h1.after(form);
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const url = new URL(window.location.href);
+        url.searchParams.append('_dux_edit', '1');
+        window.location = url.toString();
+      });
+    }
+
+
   }
 
   addEventListener('DOMContentLoaded', initialize);
